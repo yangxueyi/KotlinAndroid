a8f3a6da71494cf5d443fde618697446feeddcdf
yangxueyi <646747453@qq.com>
2018-01-31 20:06:02 +0800

    hotAdapter

---
+++ app/src/main/java/com/xueyi/yang/kotlinandroid/adapter/HotAdapter.kt
@@ -0,0 +1,20 @@
+package com.xueyi.yang.kotlinandroid.adapter
+
+import android.content.Context
+import android.view.View
+import com.xueyi.yang.kotlinandroid.bean.HotKeyResponse
+import com.xueyi.yang.kotlinandroid.module.welcome.contract.WelcomeContract
+import com.zhy.view.flowlayout.FlowLayout
+import com.zhy.view.flowlayout.TagAdapter
+
+/**
+ * Created by Administrator on 2018/1/31.
+ */
+class HotAdapter(val context : Context ,  hotLists : MutableList<HotKeyResponse.Data>) {
+    /*: TagAdapter<HotKeyResponse.Data>(hotLists) {
+    override fun getView(parent: FlowLayout?, position: Int, data: HotKeyResponse.Data?): View {
+
+        return
+    }*/
+
+}
